b522bf5bb3a7567eb4bf28f9c06609c60e469895
michaelmagistro <mikemagistro@gmail.com>
2023-06-14 21:17:44 -0600

    add unit tests for xpath_utils functions and general_utils functions

--- test_webspectre.py
+++ test_webspectre.py
@@ -1,5 +1,7 @@
 from scrapy.http import HtmlResponse
 from webspectre import WebSpectreSpider
+import xpath_utils as xpu
+import general_utils as gu
 
 def create_mock_response():
     # create a mock response object
@@ -24,5 +26,21 @@ class TestWebspectre:
 
     def test_total_element_count(self):
         # check that the total elements in the selector list is equal to the number of elements in the test html file
-        print(self.result.css("*"))
         assert len(self.result.css("*")) == 89
+
+    def test_get_full_xpath_list(self):
+        # check that the length of the full xpath list is 93
+        assert len(xpu.get_full_xpath_list(self.result)) == 93
+
+    def test_get_max_depth(self):
+        # check that the max depth of the tree is 5
+        assert xpu.get_max_depth(self.result)[0] == 10
+    
+    def test_get_unique_tags_count(self):
+        # check that the number of unique tags is 15
+        print(xpu.get_unique_tags_count(self.result))
+        assert len(xpu.get_unique_tags_count(self.result)) == 22
+
+    def test_get_sum(self):
+        # check that the sum of the values in the unique tags dictionary is 89
+        assert gu.get_sum(xpu.get_unique_tags_count(self.result).values()) == 89
